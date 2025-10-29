1d9414e4f5597aa42c069762a460b6bdeea35c8c
João Pedro Floriano <joaopedrofloriano21@gmail.com>
2025-10-28 23:20:56 -0300

    Fechos feitos: transitivo, reflexivo e simétrico

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         Matrizes matriz = new Matrizes();
-        matriz.propriedades();
+        //matriz.propriedades();
+
+        int[][] Ms = matriz.closureSymmetric(matriz.M_metro);
+        matriz.printMatrix("MS", Ms);
+        int[][] Mt = matriz.closureTransitive(matriz.M_metro);
+        matriz.printMatrix("Mt", Mt);
+        int[][] Mr = matriz.closureReflexive(matriz.M_metro);
+        matriz.printMatrix("Mr", Mr);
+
     }
 }
