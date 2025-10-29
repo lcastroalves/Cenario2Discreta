4272143089255edf4848186bde8bdf8683ff588f
João Pedro Floriano <joaopedrofloriano21@gmail.com>
2025-10-28 23:24:32 -0300

    Revert "Fechos feitos: transitivo, reflexivo e simétrico"
    This reverts commit 1d9414e4f5597aa42c069762a460b6bdeea35c8c.

--- src/Matrizes.java
+++ src/Matrizes.java
@@ -1,6 +1,6 @@
 public class Matrizes {
     //Matriz adjacente
-/*
+
     private int[][] A = {
             {0, 1, 0, 0, 0, 0},
             {1, 0, 1, 0, 0, 0},
@@ -9,34 +9,15 @@ public class Matrizes {
             {0, 0, 0, 0, 0, 1},
             {0, 1, 0, 1, 1, 0}
     };
-*/
-    public final int[][] M_metro = {
-            {1, 1, 0, 0, 0, 0},
-            {0, 1, 1, 0, 0, 0},
-            {0, 1, 1, 0, 0, 0},
-            {0, 1, 0, 1, 0, 1},
-            {1, 1, 0, 0, 1, 0},
-            {0, 0, 0, 0, 1, 1}
-    };
-    private final int[][] M_onibus = {
-            {0, 1, 0, 0, 0, 1},
-            {1, 1, 0, 0, 1, 0},
-            {0, 0, 1, 0, 0, 0},
-            {0, 0, 0, 1, 1, 0},
-            {0, 0, 1, 0, 0, 1},
-            {1, 1, 0, 1, 1, 0}
-    };
 
+    public void propriedades(){
+        System.out.println("Reflexiva?      " + (isReflexive(A)      ? "Sim" : "Não"));
+        System.out.println("Simétrica?      " + (isSymmetric(A)      ? "Sim" : "Não"));
+        System.out.println("Antissimétrica? " + (isAntisymmetric(A)  ? "Sim" : "Não"));
+        System.out.println("Assimétrica?    " + (isAsymmetric(A)     ? "Sim" : "Não"));
+        System.out.println("Transitiva?     " + (isTransitive(A)     ? "Sim" : "Não"));
+    }
 
-    /*
-        public void propriedades(){
-            System.out.println("Reflexiva?      " + (isReflexive(A)      ? "Sim" : "Não"));
-            System.out.println("Simétrica?      " + (isSymmetric(A)      ? "Sim" : "Não"));
-            System.out.println("Antissimétrica? " + (isAntisymmetric(A)  ? "Sim" : "Não"));
-            System.out.println("Assimétrica?    " + (isAsymmetric(A)     ? "Sim" : "Não"));
-            System.out.println("Transitiva?     " + (isTransitive(A)     ? "Sim" : "Não"));
-        }
-    */
     public boolean isReflexive(int[][] M){
         for (int i = 0; i < M.length; i++) {
             if (M[i][i] != 1) return false;
@@ -90,70 +71,4 @@ public class Matrizes {
         }
         return true;
     }
-//====================================================================================================================
-
-    //só para ter uma cópia que possa ser alterada
-    private int[][] deepCopy(int[][] original) {
-        int n = original.length;
-        int[][] copy = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            System.arraycopy(original[i], 0, copy[i], 0, n);
-        }
-        return copy;
-    }
-
-    public int[][] closureReflexive(int[][] M) {
-        int n = M.length;
-        int[][] Mr = deepCopy(M);
-
-        for (int i = 0; i < n; i++) {
-            Mr[i][i] = 1;
-        }
-        return Mr;
-    }
-    public int[][] closureTransitive(int[][] M) {
-        int n = M.length;
-        int[][] Mt = deepCopy(M);
-
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (Mt[i][k] == 1 && Mt[k][j] == 1) {
-                        Mt[i][j] = 1;
-                    }
-                }
-            }
-        }
-        return Mt;
-    }
-    public int[][] closureSymmetric(int[][] M) {
-        int n = M.length;
-        int[][] Ms = deepCopy(M);
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (M[i][j] == 1) {
-                    Ms[j][i] = 1;
-                }
-            }
-        }
-        return Ms;
-    }
-    public void printMatrix(String title, int[][] M) {
-        System.out.println("\n" + title + ":");
-        String[] stations = {"Av", "C", "Pç", "Pq", "Sh", "T"};
-        System.out.print("   ");
-        for (String s : stations) {
-            System.out.print(s + " ");
-        }
-        System.out.println();
-        for (int i = 0; i < M.length; i++) {
-            System.out.print(stations[i] + " |");
-            for (int val : M[i]) {
-                System.out.print(" " + val);
-            }
-            System.out.println(" |");
-        }
-    }
-
 }
